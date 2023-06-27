2eda84f9028a8a57097878d73f739b6fd5f9b920
SamuelMayo <samuelmayo456@gmail.com>
2023-06-27 17:06:46 -0400

    fix: remove commets

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 import express from 'express';
 
-// settings
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-
-// Start server
 const server = app.listen(PORT,()=>{console.log(`listening in PORT ${PORT}`)})
 
