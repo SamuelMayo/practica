6c05c19d9198ea55d0331f5500d0956a29423199
SamuelMayo <samuelmayo456@gmail.com>
2023-07-03 10:03:52 -0400

    feat: Install ESlint

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 import express from 'express';
-import sequelize from './database/db.js';
+import sequelize from './database/db';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const server = app.listen(PORT,()=>{
+const server = app.listen(PORT, () => {
     console.log(`listening in PORT ${PORT}`)
 
 
-    sequelize.sync({forec:true}).then(()=>{
+    sequelize.sync({ forec: true }).then(() => {
         console.log('DB conected');
-    }).catch(err=>{
+    }).catch(err => {
         console.log(`Erro: ${err}`);
     })
 })
